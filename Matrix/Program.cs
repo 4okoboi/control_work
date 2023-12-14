22be92323bc49a1c27c394ec2d9d7283157ced9b
4okoboi <74819042+4okoboi@users.noreply.github.com>
2023-12-14 19:10:56 +0300

    saved

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,4 +1,4 @@
-﻿using MatrixOOP;
+using MatrixOOP;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +37,27 @@ namespace MatrixOOP
         }
 
         public Matrix(int size) : this(size, size) { }
+
+        public Matrix(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+
+            string[] dimensions = lines[0].Split(' ');
+            int n = int.Parse(dimensions[0]);
+            int m = int.Parse(dimensions[1]);
+            this.rows = n;
+            this.columns = m;
+            matrix = new int[n, m];
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                string[] elements = lines[i].Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i - 1, j] = int.Parse(elements[j]);
+                }
+            }
+        }
 
         public void FillRandom()
         {
@@ -100,6 +121,62 @@ namespace MatrixOOP
             }
             return res;
         }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.columns != b.rows)
+                throw new ArgumentException("Умножаются только матрицы размерностей m x n и n x k");
+            var res = new Matrix(a.rows, b.columns);
+            for (int i = 0; i < a.rows; i++)
+                for (int j = 0; j < b.columns; j++)
+                    for (int k = 0; k < b.rows; k++)
+                        res[i, j] += a[i, k] * b[k, j];
+
+            return res;
+        }
+
+        public void ReplacePrimesInMatrix()
+        {
+            if (rows != columns)
+                throw new InvalidOperationException("матрица должна быть квадратной для проверки главной диагонали");
+            bool isAscendingDiagonal = true;
+            for (int i = 0; i < rows - 1; i++)
+            {
+                if (matrix[i, i] >= matrix[i + 1, i + 1])
+                {
+                    isAscendingDiagonal = false;
+                    break;
+                }
+            }
+            if (isAscendingDiagonal)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (IsPrime(matrix[i, j]))
+                        {
+                            matrix[i, j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
 
@@ -113,4 +190,3 @@ public class Program
         Console.WriteLine(m1);
     }
 }
-
